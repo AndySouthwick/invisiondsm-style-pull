8540480874ec68f94376d918f092b2bff9a3ce82
Andrew Southwick <Andrew.Southwick@us.usana.com>
2018-11-01 16:23:06 -0600

    removed commented code

--- index.js
+++ index.js
@@ -8,20 +8,6 @@ const dataArray = [
   {"url": "https://projects.invisionapp.com/dsm-export/usana-dsm/usana-design-system/style-params.styl?key=Sye_KlJfCf",
     "file": "node_modules/invision-dsm-style-renderer/style-params.styl"},
 ]
-// module.exports.renderStyles = () => {
-//   dataArray.map(data => {
-//     rp(data.url)
-//       .then(e => {
-//         console.log(String(e))
-//         let options = {encoding: 'UTF8'};
-//         let writeStream = fs.createWriteStream(data.file, options);
-//         writeStream.write(String(e))
-//         writeStream.on('finish', () => {
-//           console.log('wrote all data to file')
-//         })
-//       })
-//   })
-// }
  module.exports.renderStyles = async () => {
   await dataArray.map(async data => {
     const e = await rp(data.url)
