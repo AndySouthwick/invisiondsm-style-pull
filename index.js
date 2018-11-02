67555932ab770550dc209e76630e051cd856e2e3
Andrew Southwick <Andrew.Southwick@us.usana.com>
2018-11-02 11:44:28 -0600

    modified variables to reflect better what the item is

--- index.js
+++ index.js
@@ -2,18 +2,18 @@ const rp = require('request-promise');
 const fs = require('fs');
 const dataArray = [
   {"url": "<YOUR SCSS URL GOES HERE>",
-    "file": "style-params.scss"},
+    "location": "style-params.scss"},
   {"url": "<YOUR LESS URL GOES HERE>",
-    "file": "style-params.less"},
+    "location": "style-params.less"},
   {"url": "<YOUR STYLUS URL GOES HERE>",
-    "file": "style-params.styl"},
+    "location": "style-params.styl"},
 ]
  module.exports.renderStyles = async () => {
   await dataArray.map(async data => {
     const e = await rp(data.url)
         console.log(String(e))
         let options = {encoding: 'UTF8'};
-        let writeStream = fs.createWriteStream(data.file, options);
+        let writeStream = fs.createWriteStream(data.location, options);
         writeStream.write(String(e))
         writeStream.on('finish', () => {
           console.log('wrote all data to file')
